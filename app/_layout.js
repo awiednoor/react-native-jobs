b880bac8d5f17fe1e002d83c3aa61491f7200338
Noor Awied <nooorawied@gmail.com>
2023-04-07 08:46:41 +0300

    finished main header components styling for welcome screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+//for using cutom fonts
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+//Makes the splash screen remain visible until the hide method is called
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMeduim: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      //Hides the splash screen after the app is loaded, then shows the homepage
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
