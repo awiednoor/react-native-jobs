a976d27ad8fd3707b0c0d6c690781ec554fdad48
Noor Awied <nooorawied@gmail.com>
2023-05-03 13:34:02 +0300

    :sparkles: added search functionality

--- app/index.js
+++ app/index.js
@@ -11,6 +11,7 @@ import {
 const Home = () => {
   //define router using the router hook
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -38,7 +39,13 @@ const Home = () => {
       />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={{ flex: 1, padding: SIZES.medium }}>
-          <Welcome />
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              searchTerm ? router.push(`/search/${searchTerm}`) : null;
+            }}
+          />
           <Popularjobs />
           <Nearbyjobs />
         </View>
